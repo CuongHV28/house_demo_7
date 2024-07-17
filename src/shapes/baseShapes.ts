ca6a929e9af6b055022aaa33e57821679933ae21
cuong <cuonghv@vnpt.vn>
2024-07-17 17:56:38 +0700

    eeeee

--- src/shapes/baseShapes.ts
+++ src/shapes/baseShapes.ts
@@ -14,7 +14,7 @@ export interface IHoleSettings {
     height: number; // height of the hole as a percentage of the wall height
     depth: number;  // depth of the hole
     offsetLeft?: number; // offset of the hole from the left side of the wall as a percentage of the wall width
-    offsetTop?: number; // offset of the hole from the top side of the wall as a percentage of the wall height
+    offsetGround?: number; // offset of the hole from the top side of the wall as a percentage of the wall height
 }
 
 export interface IWallSettings {
@@ -36,9 +36,27 @@ export interface IDoorSettings extends IHoleSettings {
 }
 
 export interface IWindowSettings extends IHoleSettings {
-    
+
 }
 
 export interface IStairsSettings {
     // Define the properties of the 'IStairsSettings' interface here
+}
+
+
+function addWallWithDoorAndWindow(
+    wallSettings: IWallSettings,
+    doorSettings: IDoorSettings[],
+    windowSettings: IWindowSettings[]
+) :THREE.Mesh {
+    // Define the function 'addWallWithDoorAndWindow' here
+    const wall = new Operation(new THREE.BoxGeometry(wallSettings.width, wallSettings.height, wallSettings.depth));
+    let holes = [];
+    doorSettings.forEach((door) => {
+        let doorHole = new Operation(new THREE.BoxGeometry(door.width, door.height, door.depth));
+        
+        holes.push(new Operation(new THREE.BoxGeometry(door.width, door.height, door.depth)));
+    });
+
+    return new THREE.Mesh();
 }
